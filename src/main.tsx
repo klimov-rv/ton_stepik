7fe5919d9b2236a5eb67e75f3a22029895c49c4c
klimov-rv <klimov.rv@yandex.ru>
2026-01-10 15:09:45 +0300

    adapt to my repo

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = "https://evilcore29.github.io/ton_stepik_fe/manifest.json";
+const manifestUrl = 'https://klimovproject.online/manifest.json';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </TonConnectUIProvider>
+  </TonConnectUIProvider>,
 );
